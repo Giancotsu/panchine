ca5783b4aeb623cf8242272059f18dc1c7971ffb
Gianfranco Larocca <97807263+Giancotsu@users.noreply.github.com>
2024-03-05 17:07:00 +0100

    https

--- src/app/servizi/url.service.ts
+++ src/app/servizi/url.service.ts
@@ -7,7 +7,7 @@ export class UrlService {
 
   public localhost="http://localhost:9000"
   //public remotehost="https://panchine.up.railway.app"
-  public remotehost="http://93.48.62.240:8080"
+  public remotehost="https://93.48.62.240:8080"
 
 
 }
